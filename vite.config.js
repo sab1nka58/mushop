f1894ee7d6e3c5574e742e32acb9061bcb079acd
sabina <sabina05@gmail.com.>
2025-02-15 20:53:27 +0500

    Настроил деплой на GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [react(),
   base: process.env.NODE_ENV === 'production' ? '/MyShop/' : '/'
+  ],
+
 })
